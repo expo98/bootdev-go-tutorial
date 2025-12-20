228be90c3ba9707c24812deab8d9bb2aec511d2c
expo98 <miguelmpereira0409@gmail.com>
2025-12-20 20:42:33 +0000

    L3: The Error Interface

---
+++ CH6: Errors/L3/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type divideError struct {
+	dividend float64
+}
+
+func (e divideError) Error() string {
+	s := fmt.Sprintf("can not divide %v by zero", e.dividend)
+	return s
+}
+
+func divide(dividend, divisor float64) (float64, error) {
+	if divisor == 0 {
+		return 0, divideError{dividend: dividend}
+	}
+	return dividend / divisor, nil
+}
