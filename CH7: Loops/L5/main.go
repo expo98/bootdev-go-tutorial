e52548d154f59f1215a9cb715f4d16437db36b8f
expo98 <miguelmpereira0409@gmail.com>
2025-12-20 23:09:14 +0000

    Continue & Break

---
+++ CH7: Loops/L5/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func printPrimes(max int) {
+
+	for i := 2; i <= max; i++ {
+		var isPrime bool = true
+
+		if i == 2 {
+			fmt.Println(i)
+		}
+
+		if i%2 == 0 {
+			continue
+		}
+
+		for j := 3; j*j < i; j = j + 2 {
+			if i%j == 0 {
+				isPrime = false
+				break
+			}
+		}
+
+		if isPrime == true {
+			fmt.Println(i)
+		}
+	}
+}
+
+// don't edit below this line
+
+func test(max int) {
+	fmt.Printf("Primes up to %v:\n", max)
+	printPrimes(max)
+	fmt.Println("===============================================================")
+}
+
+func main() {
+	test(10)
+	test(20)
+	test(30)
+}
